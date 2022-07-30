7d9232a6339305f7ddc02b0b2a596429de81c315
alex-dll <alexsandrojr.303@gmail.com>
2022-07-30 19:05:48 -0300

    feat: :sparkles: Introduce nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,18 @@
 import { AnimateSharedLayout } from 'framer-motion';
 import { AppProps } from 'next/app';
+import Router from 'next/router';
+import NProgress from 'nprogress';
 
 import { Layout } from '../components';
 
 import '../styles/global.css';
+import '../styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => {
+  NProgress.start();
+});
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
